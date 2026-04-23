f9446a289ea586140762fc29094450fb66c81813
Daksh  Ghadia <dakshghadia@Dakshs-MacBook-Air.local>
2026-04-23 18:16:49 -0400

    made changes

--- km-app/lib/db.js
+++ km-app/lib/db.js
@@ -1,14 +1,13 @@
 import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool({
-  user:             process.env.DB_APP_USER     || process.env.DB_USER,
-  password:         process.env.DB_APP_PASSWORD || process.env.DB_PASSWORD,
-  database:         process.env.DB_NAME,
-  ...(process.env.INSTANCE_UNIX_SOCKET
-    ? { socketPath: process.env.INSTANCE_UNIX_SOCKET }
-    : { host: process.env.DB_HOST, ssl: { rejectUnauthorized: false } }),
-  waitForConnections: true,
-  connectionLimit:    10,
-});
+const dbConfig = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  // Hardcoding this temporarily to guarantee it uses the secure tunnel
+  socketPath: '/cloudsql/kings-market-491600:us-east4:kings-market-db'
+};
 
-export default pool;
+const db = mysql.createPool(dbConfig);
+
+export default db;
