ca4f914a21ca42b8fa88d8057280a29ad9a4d3e1
njdelapaz <40581668+njdelapaz@users.noreply.github.com>
2026-04-24 02:51:36 -0400

    db.js edit for proxy use

--- km-app/lib/db.js
+++ km-app/lib/db.js
@@ -4,10 +4,15 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  // Hardcoding this temporarily to guarantee it uses the secure tunnel
-  socketPath: '/cloudsql/kings-market-491600:us-east4:kings-market-db',
-  enableCleartextPlugin: true
 };
+
+if (process.env.DB_SOCKET_PATH) {
+  dbConfig.socketPath = process.env.DB_SOCKET_PATH;
+  dbConfig.enableCleartextPlugin = true;
+} else {
+  dbConfig.host = process.env.DB_HOST || '127.0.0.1';
+  dbConfig.port = parseInt(process.env.DB_PORT || '3306', 10);
+}
 
 const db = mysql.createPool(dbConfig);
 
